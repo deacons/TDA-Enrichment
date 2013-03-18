f040f6ab36d598f045cf93c9637e4331d6740ae6
George Garside <grg.garside@gmail.com>
2013-03-18 19:13:46 +0000

    Update TestFlightSDK 1.2.3-beta to 1.2.4-beta

--- TestFlightSDK1.2.4-beta/TestFlight.h
+++ TestFlightSDK1.2.4-beta/TestFlight.h
@@ -6,7 +6,7 @@
 //  Copyright 2011 TestFlight. All rights reserved.
 
 #import <Foundation/Foundation.h>
-#define TESTFLIGHT_SDK_VERSION @"1.2.3-beta"
+#define TESTFLIGHT_SDK_VERSION @"1.2.4-beta"
 #undef TFLog
 
 #if __cplusplus
