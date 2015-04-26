1545c72a04496cd882effebaa87e3c00409d3932
George Garside <grg.garside@gmail.com>
2015-04-26 16:43:37 +0100

    Add Google Analytics plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.danielcwilson.plugins.googleanalytics/www/analytics.js",
+        "id": "com.danielcwilson.plugins.googleanalytics.UniversalAnalytics",
+        "clobbers": [
+            "analytics"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.danielcwilson.plugins.googleanalytics": "0.7.0"
+}
 // BOTTOM OF METADATA
 });
