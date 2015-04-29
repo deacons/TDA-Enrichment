7a202f1a0210866b378f7c54e80de81b8b818438
George Garside <grg.garside@gmail.com>
2015-04-29 17:08:36 +0100

    Add com.google.cordova.admob plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,21 @@ module.exports = [
         "clobbers": [
             "analytics"
         ]
+    },
+    {
+        "file": "plugins/com.google.cordova.admob/www/AdMob.js",
+        "id": "com.google.cordova.admob.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.danielcwilson.plugins.googleanalytics": "0.7.0"
+    "com.danielcwilson.plugins.googleanalytics": "0.7.0",
+    "com.google.cordova.admob": "2.7.5",
+    "com.rjfun.cordova.extension": "1.0.6"
 }
 // BOTTOM OF METADATA
 });
