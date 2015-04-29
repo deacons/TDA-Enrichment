837faf20ddb9e6a4d27aba99556287f133a81798
George Garside <grg.garside@gmail.com>
2015-04-29 17:07:14 +0100

    Implement Google Analytics tracking

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,14 @@
 'use strict';
 
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    console.log('onDeviceReady');
+    window.analytics.debugMode();
+    window.analytics.startTrackerWithId('UA-44246405-6');
+    window.analytics.trackView('index');
+}
+
 // Initialize your app
 var myApp = new Framework7({
     animateNavBackIcon: true
@@ -10,4 +19,9 @@ var $$ = Dom7;
 
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
+});
+
+$$(document).on('pageInit', function (p) {
+    var pageinfo = p.detail.page;
+    console.log(pageinfo.name);
 });
