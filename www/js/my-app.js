958c62c9e8d71c8e994f4add0160049ed9abc27c
George Garside <grg.garside@gmail.com>
2015-04-29 17:23:43 +0100

    Implement AdMob

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,11 +2,26 @@
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var admobid = {};
+
 function onDeviceReady() {
     console.log('onDeviceReady');
+    // Google Analytics
     window.analytics.debugMode();
     window.analytics.startTrackerWithId('UA-44246405-6');
     window.analytics.trackView('index');
+    // AdMob
+    if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+        admobid = {
+            banner: 'ca-app-pub-9733787518596382/1681312032',
+            interstitial: 'ca-app-pub-9733787518596382/2308277237'
+        };
+    }
+    if (AdMob) AdMob.createBanner ({
+        adId: admobid.banner,
+        position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        autoShow: true
+    });
 }
 
 // Initialize your app
